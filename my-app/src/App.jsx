7b9415ceb2c0f700b22f22643da1fa38c56eea5a
apurvayeole <apurvayeole56@gmail.com>
2026-06-22 01:34:06 +0530

    Added State

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Greeting from './components/Greeting.jsx';
-import City from './components/City.jsx'
+import City from './components/City.jsx';
+import State from './components/State.jsx'
 
 function App(){
     return(
         <div>
         <Greeting></Greeting>
         <City/>
+        <State/>
         </div>
     )
 }
---
+++ my-app/src/components/State.jsx
@@ -0,0 +1,6 @@
+function State() {
+    return(
+        <h1> ON! </h1>
+    );
+}
+export default State;
