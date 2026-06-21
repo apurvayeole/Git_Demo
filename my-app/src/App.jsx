a22e6472d076cdc89060bb1d4fe7397d9799a1a7
apurvayeole <apurvayeole56@gmail.com>
2026-06-21 23:02:10 +0530

    Initial Commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from './components/Greeting.jsx';
+
+function App(){
+    return(
+        <div>
+        <Greeting>HELLO APP</Greeting>
+        </div>
+    )
+}
+
+export default App;
---
+++ my-app/src/components/Greeting.jsx
@@ -0,0 +1,6 @@
+function Greeting() {
+    return(
+        <h1> HELLO! </h1>
+    );
+}
+export default Greeting;
